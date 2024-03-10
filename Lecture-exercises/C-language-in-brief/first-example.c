aba2f9c4cb7921f3b55b787b86ec97ef3304efb1
Lenovo <yousef.zare2000@gmail.com>
2024-03-10 16:49:09 +0100

    creation

---
+++ Lecture-exercises/C-language-in-brief/first-example.c
@@ -0,0 +1,20 @@
+//
+// Created by Lenovo on 3/10/2024.
+//
+#include <stdio.h>
+
+int main() {
+    printf("%d\n", 3 + 4); // print 7
+
+    // print Hello world! on two lines
+    printf("Hello\n");
+    printf("world!\n");
+
+
+    // print multiple values
+    // with a single print function call
+    printf("My numbers are %d and %d\n", 3 + 4, 3 + 10); // can be on multiple lines
+    // print  messages with empty line in between
+    printf("Goodbye\n\nHope to see you again\n");
+    return 0;
+}
---
+++ Lecture-exercises/C-language-in-brief/second-example.c
@@ -0,0 +1,24 @@
+//
+// Created by Lenovo on 3/10/2024.
+//
+/*
+ This program simulates an elevator panel
+ that skips the 13th floor. */
+#include <stdio.h>
+
+int main (void) {
+    // declare/define variables
+    int floor,
+            actualFloor;
+    // Obtain floor number from the user as an integer
+    printf ("Floor: "); scanf ("%d", &floor);
+
+    /* Adjust floor if necessary. */
+    if (floor > 13) actualFloor = floor - 1;
+    else actualFloor = floor;
+
+    // Print the result.
+    printf("The elevator will travel to the ");
+    printf("actual floor %d\n", actualFloor);
+    return 0;
+}
